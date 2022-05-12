0d1faeae2244f42cb45a06d6d603b1db1902aa75
Bocco1 <Jacopo@DESKTOP-OBNKPAL>
2022-05-12 16:16:26 +0200

    versione 0

---
+++ BoccoJacopoIngSw/TrapezioBocco/src/src/MainClass.java
@@ -0,0 +1,23 @@
+package src;
+
+
+public class MainClass {
+
+	public static void main(String[] args) 
+	{
+		
+			Trapezio t1,t2,t3;
+			
+			t1 = new Trapezio(7,8,5);
+			t2 = new Trapezio(6,7,3);
+			t3 = new Trapezio(9,3,1);
+			
+			System.out.println(t1.area());
+			System.out.println(t2.area());
+			System.out.println(t3.area());
+			
+		
+
+	}
+
+}
