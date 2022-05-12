04784519bfb704316ba04743be8bdee27e8316fa
Jacopo <Jacopo@DESKTOP-OBNKPAL>
2022-05-12 17:50:53 +0200

    versione 2 gitHub

---
+++ BoccoJacopo/BoccoJacopo/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"BoccoJacopoClass","l":"BoccoJacopoClass()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"BoccoJacopoClass","l":"BoccoMet(int)"},{"p":"<Unnamed>","c":"BoccoJacopoClass","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
